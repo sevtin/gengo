ac8c3f7aa2848fa3a390c3b6b1262d62dd8ae601
huchaojun <huchaojun@topgamesinc.com>
2024-06-07 14:55:23 +0800

    mysql 更新

--- lark/pkg/common/xmysql/mysql.go
+++ lark/pkg/common/xmysql/mysql.go
@@ -6,8 +6,11 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"lark/pkg/common/xlog"
 	"lark/pkg/conf"
+	"log"
+	"os"
 	"time"
 )
 
@@ -86,9 +89,20 @@ func ConnectDB(cfg *conf.Mysql) (db *gorm.DB, err error) {
 		cfg.Address,
 		cfg.Db)
 
+	// 定义日志配置
+	gormLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second,                   // 慢查询阈值
+			LogLevel:                  logger.LogLevel(cfg.LogLevel), // 日志级别
+			IgnoreRecordNotFoundError: true,                          // 忽略记录未找到错误
+			Colorful:                  false,                         // 禁用彩色打印
+		},
+	)
 	opts = &gorm.Config{
 		SkipDefaultTransaction: false, // 禁用默认事务(true: Error 1295: This command is not supported in the prepared statement protocol yet)
 		PrepareStmt:            false, // 创建并缓存预编译语句(true: Error 1295)
+		Logger:                 gormLogger,
 	}
 
 	db, err = gorm.Open(mysql.Open(dsn), opts)
@@ -96,9 +110,7 @@ func ConnectDB(cfg *conf.Mysql) (db *gorm.DB, err error) {
 		xlog.Error(err.Error())
 		return
 	}
-	if cfg.Debug == true {
-		db = db.Debug()
-	}
+
 	sqlDB, err = db.DB()
 	if err != nil {
 		xlog.Error(err.Error())
@@ -131,10 +143,3 @@ func Connected() bool {
 	}
 	return cli.connected
 }
-
-/*
-SetMaxOpenConns：设置池中与数据打开的最大连接数，默认不限制连接数量。一般来说，该值设置的越大，可以并发执行的数据库查询就越多。
-SetMaxIdleConns：设置池中最大空闲连接数，默认值是2. 理论上有更多的空闲连接可以减少从头建立新连接的概率，建立连接的过程比较耗时。但是过多的空闲连接会浪费内存占用。如果一个连接空闲时间过长，它也可能变得不可用。MySQL默认会自动关闭8小时未使用的连接。
-SetConnMaxIdleTime：设置池中连接在关闭之前可用空闲的最长时间，默认是不限制时间。如果设置为2小时，表示池中自上次使用以后在池中空闲了2小时的连接将标为过期被清理。
-SetConnMaxLifetime：设置池中连接关闭前可以保持打开的最长时间，默认是不限制时间。
-*/
--- lark/pkg/entity/mysql.go
+++ lark/pkg/entity/mysql.go
@@ -2,17 +2,16 @@ package entity
 
 import (
 	"fmt"
-	"gorm.io/gorm"
 	"reflect"
 	"time"
 )
 
 type GormCreatedTs struct {
-	CreatedTs int64 `gorm:"column:created_ts;autoCreateTime;NOT NULL" json:"created_ts"`
+	CreatedTs int64 `gorm:"column:created_ts;autoCreateTime:milli;NOT NULL" json:"created_ts"`
 }
 
 type GormUpdatedTs struct {
-	UpdatedTs int64 `gorm:"column:updated_ts;autoUpdateTime;NOT NULL" json:"updated_ts"`
+	UpdatedTs int64 `gorm:"column:updated_ts;autoUpdateTime:milli;NOT NULL" json:"updated_ts"`
 }
 
 type GormDeletedTs struct {
@@ -20,44 +19,26 @@ type GormDeletedTs struct {
 }
 
 type GormEntityTs struct {
-	CreatedTs int64 `gorm:"column:created_ts;autoCreateTime;NOT NULL" json:"created_ts"`
-	UpdatedTs int64 `gorm:"column:updated_ts;autoUpdateTime;NOT NULL" json:"updated_ts"`
+	CreatedTs int64 `gorm:"column:created_ts;autoCreateTime:milli;NOT NULL" json:"created_ts"`
+	UpdatedTs int64 `gorm:"column:updated_ts;autoUpdateTime:milli;NOT NULL" json:"updated_ts"`
 	DeletedTs int64 `gorm:"column:deleted_ts;default:0;NOT NULL" json:"deleted_ts"`
 }
 
 type GormTs struct {
-	CreatedTs int64 `gorm:"column:created_ts;autoCreateTime;NOT NULL" json:"created_ts"`
-	UpdatedTs int64 `gorm:"column:updated_ts;autoUpdateTime;NOT NULL" json:"updated_ts"`
+	CreatedTs int64 `gorm:"column:created_ts;autoCreateTime:milli;NOT NULL" json:"created_ts"`
+	UpdatedTs int64 `gorm:"column:updated_ts;autoUpdateTime:milli;NOT NULL" json:"updated_ts"`
 }
 
 func Deleted() (column string, value interface{}) {
-	return "deleted_ts", time.Now().Unix()
+	return "deleted_ts", time.Now().UnixMilli()
 }
 
-type MysqlQuery struct {
-	Model  interface{}
-	Fields string
-	Query  string
-	Args   []interface{}
-	Limit  int
-	Offset int
-	Sort   string
+type Mysql struct {
+	Query string
+	Args  []interface{}
 }
 
-func NewMysqlQuery() *MysqlQuery {
-	return &MysqlQuery{
-		Query: "deleted_ts=0",
-		Args:  make([]interface{}, 0),
-	}
-}
-
-func NewNormalQuery() *MysqlQuery {
-	return &MysqlQuery{
-		Args: make([]interface{}, 0),
-	}
-}
-
-func (m *MysqlQuery) andCondition(condition string) {
+func (m *Mysql) andCondition(condition string) {
 	if m.Query == "" {
 		m.Query = condition
 	} else {
@@ -65,123 +46,71 @@ func (m *MysqlQuery) andCondition(condition string) {
 	}
 }
 
-func (m *MysqlQuery) orCondition(condition string) {
+func (m *Mysql) orCondition(condition string) {
 	if m.Query == "" {
 		m.Query = condition
 	} else {
-		m.Query += " Or " + condition
+		m.Query = "(" + m.Query + ") OR (" + condition + ")"
 	}
 }
 
-func (m *MysqlQuery) NotDeleted(alias string) {
+func (m *Mysql) NotDeleted(alias string) {
 	clause := alias + ".deleted_ts=0"
 	m.andCondition(clause)
 }
 
-func (m *MysqlQuery) IsNull(field string) {
+func (m *Mysql) IsNull(field string) {
 	clause := field + " IS NULL"
 	m.andCondition(clause)
 }
 
-func (m *MysqlQuery) IsNotNull(field string) {
+func (m *Mysql) IsNotNull(field string) {
 	clause := field + " IS NOT NULL"
 	m.andCondition(clause)
 }
 
-func (m *MysqlQuery) SetFilter(query string, value ...interface{}) {
+func (m *Mysql) Where(query string, value ...interface{}) {
 	m.andCondition(query)
 	m.Args = append(m.Args, value...)
 }
 
-func (m *MysqlQuery) SetFilterOr(query string, value ...interface{}) {
+func (m *Mysql) OrWhere(query string, value ...interface{}) {
 	m.orCondition(query)
 	m.Args = append(m.Args, value...)
 }
 
-func (m *MysqlQuery) Between(field string, begin interface{}, end interface{}) {
+func (m *Mysql) Between(field string, begin interface{}, end interface{}) {
 	clause := field + " BETWEEN ? AND ?"
 	m.andCondition(clause)
 	m.Args = append(m.Args, begin)
 	m.Args = append(m.Args, end)
 }
 
-func (m *MysqlQuery) OpenParen() {
+func (m *Mysql) OpenParen() {
 	m.Query += "(1=1"
 }
 
-func (m *MysqlQuery) CloseParen() {
+func (m *Mysql) CloseParen() {
 	m.Query += ")"
 }
 
-func (m *MysqlQuery) SetSort(sort string) {
-	m.Sort = sort
-}
-
-func (m *MysqlQuery) SetOffset(offset int) {
-	m.Offset = offset
-}
-
-func (m *MysqlQuery) SetLimit(limit int32) {
-	m.Limit = int(limit)
-}
-
-func (m *MysqlQuery) AndQuery(query string) {
+func (m *Mysql) AndQuery(query string) {
 	m.andCondition(query)
 }
 
-func (m *MysqlQuery) OrQuery(query string) {
+func (m *Mysql) OrQuery(query string) {
 	m.orCondition(query)
 }
 
-func (m *MysqlQuery) SetLink(link string) {
+func (m *Mysql) SetLink(link string) {
 	m.Query += fmt.Sprintf(" %s ", link)
 }
 
-func (m *MysqlQuery) AppendArg(value interface{}) {
+func (m *Mysql) AppendArg(value interface{}) {
 	m.Args = append(m.Args, value)
 }
 
-func (m *MysqlQuery) Reset() {
-	m.Model = nil
-	m.Fields = ""
-	m.Query = "deleted_ts=0"
-	m.Args = make([]interface{}, 0)
-	m.Sort = ""
-	m.Limit = 0
-	m.Offset = 0
-}
-
-func (m *MysqlQuery) Normal() {
-	m.Model = nil
-	m.Fields = ""
-	m.Query = ""
-	m.Args = make([]interface{}, 0)
-	m.Sort = ""
-	m.Limit = 0
-	m.Offset = 0
-}
-
-func (m *MysqlQuery) Find(db *gorm.DB, dist interface{}) (err error) {
-	err = db.Model(m.Model).Select(m.Fields).Where(m.Query, m.Args...).Find(dist).Error
-	return
-}
-
-func (m *MysqlQuery) Finds(db *gorm.DB, model any) (dest any, err error) {
-	var (
-		typ        = reflect.TypeOf(model)
-		sliceType  = reflect.SliceOf(typ)
-		sliceValue = reflect.New(sliceType).Elem()
-	)
-	dest = sliceValue.Interface()
-	err = db.Model(m.Model).Select(m.Fields).Where(m.Query, m.Args...).Find(dest).Error
-	return
-}
-
-func (m *MysqlQuery) Delete(db *gorm.DB) (err error) {
-	return db.Model(m.Model).Where(m.Query, m.Args...).Update(Deleted()).Error
-}
-
-func (m *MysqlQuery) SetConditions(obj any) {
+func (m *Mysql) SetConditions(obj any) {
 	t := reflect.TypeOf(obj)
 	v := reflect.ValueOf(obj)
 	for i := 0; i < t.NumField(); i++ {
@@ -189,90 +118,7 @@ func (m *MysqlQuery) SetConditions(obj any) {
 		value := v.Field(i).Interface()
 		tag := field.Tag.Get("where")
 		if tag != "" {
-			m.SetFilter(tag+"=?", value)
-		}
-	}
-}
-
-type MysqlUpdate struct {
-	Query  string
-	Args   []interface{}
-	Values map[string]interface{}
-}
-
-func NewMysqlUpdate() *MysqlUpdate {
-	return &MysqlUpdate{
-		Args:   make([]interface{}, 0),
-		Values: make(map[string]interface{}),
-	}
-}
-
-func (m *MysqlUpdate) andCondition(condition string) {
-	if m.Query == "" {
-		m.Query = condition
-	} else {
-		m.Query += " AND " + condition
-	}
-}
-
-func (m *MysqlUpdate) orCondition(condition string) {
-	if m.Query == "" {
-		m.Query = condition
-	} else {
-		m.Query += " Or " + condition
-	}
-}
-
-func (m *MysqlUpdate) Set(key string, value interface{}) {
-	m.Values[key] = value
-}
-
-func (m *MysqlUpdate) SetFilter(query string, value ...interface{}) {
-	m.andCondition(query)
-	m.Args = append(m.Args, value...)
-}
-
-func (m *MysqlUpdate) AndQuery(query string) {
-	m.andCondition(query)
-}
-
-func (m *MysqlUpdate) AppendArg(value interface{}) {
-	m.Args = append(m.Args, value)
-}
-
-func (m *MysqlUpdate) Reset() {
-	m.Query = "deleted_ts=0"
-	m.Args = make([]interface{}, 0)
-	m.Values = make(map[string]interface{})
-}
-
-func (m *MysqlUpdate) Normal() {
-	m.Query = ""
-	m.Args = make([]interface{}, 0)
-	m.Values = make(map[string]interface{})
-}
-
-func (m *MysqlUpdate) Updates(db *gorm.DB, model interface{}) *gorm.DB {
-	return db.Model(model).Where(m.Query, m.Args...).Updates(m.Values)
-}
-
-func (m *MysqlUpdate) Delete(db *gorm.DB, model interface{}) *gorm.DB {
-	return db.Model(model).Where(m.Query, m.Args...).Update(Deleted())
-}
-
-func (m *MysqlUpdate) SetConditionsAndValues(obj any) {
-	t := reflect.TypeOf(obj)
-	v := reflect.ValueOf(obj)
-	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
-		value := v.Field(i).Interface()
-		tag := field.Tag.Get("where")
-		if tag != "" {
-			m.SetFilter(tag+"=?", value)
-		}
-		tag = field.Tag.Get("update")
-		if tag != "" {
-			m.Set(tag, value)
+			m.Where(tag+"=?", value)
 		}
 	}
 }
